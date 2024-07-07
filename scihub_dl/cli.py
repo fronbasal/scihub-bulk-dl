9398caa03d5ff2168bd85b8aef21d0e4c39f2a64
Daniel Malik <daniel.malik@mhsp.solutions>
2024-07-08 00:47:31 +0200

    feat: add to poetry

--- scihub_dl/cli.py
+++ scihub_dl/cli.py
@@ -11,11 +11,11 @@ from scihub_dl.crawler import SciHub
 @click.option('--output', '-o', type=click.Path(exists=False, file_okay=False, dir_okay=True, ), help='Output directory', required=True)
 @click.option('--url', '-u', default='https://sci-hub.se/', help='Sci-Hub URL')
 @click.option('--timeout', '-t', default=60, help='Request timeout')
-def store(doi: str, output: str, url: List[str], timeout: int):
+def main(doi: str, output: str, url: List[str], timeout: int):
     output = Path(output)
     for d in doi:
         SciHub(d, output, url, timeout).fetch()
 
 
 if __name__ == '__main__':
-    store()
+    main()
